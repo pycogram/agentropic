0b9d6214815120b3c5268ef76aa2c38cb978c475
pycogram <kaluifesinachi@yahoo.com>
2026-02-06 10:46:32 +0100

    for import

---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+//! Unified prelude for the Agentropic framework.
+
+// Core primitives
+pub use crate::core::prelude::*;
+pub use crate::messaging::prelude::*;
+pub use crate::cognition::prelude::*;
+pub use crate::patterns::prelude::*;
+pub use crate::runtime::prelude::*;
